bd89fed90cbb0b402b346fb3d8f88732bf496873
taeminHan <taemin9705@gmail.com>
2022-03-21 16:03:10 +0900

    > Logback 일시 중지 > StoreController 가게 리스트 출력 API 추가

--- NYU-Main/src/main/java/com/nyanyumserver/nyanyumserver/Controller/AdminController.java
+++ NYU-Main/src/main/java/com/nyanyumserver/nyanyumserver/Controller/AdminController.java
@@ -1,8 +1,5 @@
 package com.nyanyumserver.nyanyumserver.Controller;
 
-
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.gson.JsonObject;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,34 +14,5 @@ import java.util.List;
 
 @RestController
 public class AdminController {
-
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    @ApiOperation(value = "파이어베이스 테스트")
-    @GetMapping("/firebase")
-    public void getMessage(){
-        GoogleCredential googleCredential = null;
-        InputStream serviceAccount = null;
-        String SCOPES = "https://www.googleapis.com/auth/firebase.messaging";
-        try {
-            ClassPathResource resource = new ClassPathResource("fcm.json");
-            serviceAccount = resource.getInputStream();
-            googleCredential = GoogleCredential
-                    .fromStream(serviceAccount)
-                    .createScoped(Arrays.asList(SCOPES));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        JsonObject jsonObj = new JsonObject();
-        jsonObj.addProperty("token", "토큰정보");
-        JsonObject notification = new JsonObject();
-        notification.addProperty("body", "\uD83D\uDE03바디V1");
-        notification.addProperty("title", "\uD83D\uDE03타이틀V1");
-        jsonObj.add("notification", notification);
-        JsonObject message = new JsonObject();
-        message.add("message", jsonObj);
-    }
-
-
 
 }
--- NYU-Main/src/main/java/com/nyanyumserver/nyanyumserver/Controller/StoreController.java
+++ NYU-Main/src/main/java/com/nyanyumserver/nyanyumserver/Controller/StoreController.java
@@ -1,31 +1,47 @@
-//package com.nyanyumserver.nyanyumserver.Controller;
-//
-//import com.nyanyumserver.nyanyumserver.Service.StoreService;
-//import com.nyanyumserver.nyanyumserver.VO.PostSearchInfo;
-//import com.nyanyumserver.nyanyumserver.VO.StoreSearchInfo;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-//public class StoreController {
-//
-//    Logger logger = LoggerFactory.getLogger(this.getClass());
-//
-//    @Autowired
-//    StoreService storeService;
-//
-//    public Object getStoreList(){
-//
-//        StoreSearchInfo storeSearchInfo = new StoreSearchInfo();
-//
-//        try{
-//            logger.debug("START.");
-//
-//            storeService.getStoreList(storeSearchInfo);
-//
-//        }catch (Exception e){
-//
-//        }
-//    }
-//}
+package com.nyanyumserver.nyanyumserver.Controller;
+
+import com.nyanyumserver.nyanyumserver.Service.StoreService;
+import com.nyanyumserver.nyanyumserver.VO.StoreSearchInfo;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+
+public class StoreController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    StoreService storeService;
+
+
+    @GetMapping("/StoreList")
+    @ApiOperation(value = "가게 리스트")
+    public Object getStoreList(){
+
+        try{
+            logger.debug("START.");
+            StoreSearchInfo storeSearchInfo = new StoreSearchInfo();
+
+            storeService.getStoreList(storeSearchInfo);
+
+            logger.debug("END");
+
+            return new ResponseEntity<>(storeSearchInfo.getStoreInfos(), HttpStatus.OK);
+
+        }catch (Exception e){
+            logger.debug("ERROR, StoreList");
+
+            return new ResponseEntity<>("불러올 수 없습니다.", HttpStatus.BAD_REQUEST);
+        }
+    }
+}
