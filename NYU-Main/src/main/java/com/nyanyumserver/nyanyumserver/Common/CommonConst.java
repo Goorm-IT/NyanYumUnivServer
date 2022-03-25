bcbf2a0f9b04d669f8c39217c1139d2d108cb568
taeminHan <taemin9705@gmail.com>
2022-03-25 15:33:04 +0900

    (완료)(NYU-5)[Sprint1] DataBase MyBatis 변경
    > 기존 application.properties -> yml 로 변경 및 삭제
    > Const 관리를 위한 CommonConst 생성

---
+++ NYU-Main/src/main/java/com/nyanyumserver/nyanyumserver/Common/CommonConst.java
@@ -0,0 +1,7 @@
+package com.nyanyumserver.nyanyumserver.Common;
+
+public class CommonConst {
+    public static final String OPTION_FASTEST = "fastest";
+
+    public static final String MAIN_PATH = "/home/ubuntu/Profile/";
+}
