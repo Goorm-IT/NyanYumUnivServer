e69fcded174fcfd986c1a6890b2908ddeedcb215
olive-su <1466su@naver.com>
2022-04-13 20:29:07 +0900

    NYU-74-NYU-IMG_Swagger_Error_Fix

--- NYU-IMG/src/main/java/com/nyanyumserver/nyuimg/config/SwaggerConfig.java
+++ NYU-IMG/src/main/java/com/nyanyumserver/nyuimg/config/SwaggerConfig.java
@@ -1,10 +1,13 @@
 package com.nyanyumserver.nyuimg.config;
 
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -14,13 +17,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.HashSet;
 import java.util.Set;
 
-
+@EnableWebMvc
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig  extends WebMvcConfigurationSupport {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
-    public Docket swaggerApi() {
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("NYU-IMG")
                 .select()
@@ -29,23 +31,19 @@ public class SwaggerConfig  extends WebMvcConfigurationSupport {
                 .build();
     }
 
-    private Set<String> getConsumeContentTypes() {
-        Set<String> consumes = new HashSet<>();
-        consumes.add("application/json;charset=UTF-8");
-        consumes.add("application/x-www-form-urlencoded");
-        return consumes;
+    @ Bean
+    public InternalResourceViewResolver defaultViewResolver () {
+        return  new  InternalResourceViewResolver ();
     }
 
-    private Set<String> getProduceContentTypes() {
-        Set<String> produces = new HashSet<>();
-        produces.add("application/json;charset=UTF-8");
-        return produces;
-    }
-    
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui/index.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-        super.addResourceHandlers(registry);
+        registry
+                .addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
