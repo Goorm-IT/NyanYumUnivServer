0b01946462bb97383812e8659c33aebd5439ca32
olive-su <1466su@naver.com>
2022-04-18 10:56:27 +0900

    (진행)[NYU-61][Sprint2]NYU-Main 프로필 이미지 삭제 API 추가

--- NYU-Main/src/main/java/com/nyanyumserver/nyanyumserver/Service/impl/UserServiceImpl.java
+++ NYU-Main/src/main/java/com/nyanyumserver/nyanyumserver/Service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.sql.SQLException;
 
 
@@ -17,6 +18,7 @@ public class UserServiceImpl implements UserService{
     UserMapper userMapper;
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
     public void getLogin(UserSearchInfo userSearchInfo){
         try{
