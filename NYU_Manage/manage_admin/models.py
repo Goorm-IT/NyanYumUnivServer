df43fdbf1e1e09d54fa57c74df06e857a82aaf54
olive-su <1466su@naver.com>
2022-09-01 02:20:33 +0900

    Feat:관리자 페이지 마이그레이션

--- NYU_Manage/manage_admin/models.py
+++ NYU_Manage/manage_admin/models.py
@@ -9,82 +9,121 @@ from django.db import models
 
 
 class Comment(models.Model):
-    # Field name made lowercase.
-    commentid = models.CharField(
-        db_column='commentId', primary_key=True, max_length=50)
-    uid = models.CharField(max_length=50, blank=True, null=True)
-    # Field name made lowercase.
-    nickname = models.CharField(
-        db_column='nickName', max_length=50, blank=True, null=True)
-    text = models.CharField(max_length=100, blank=True, null=True)
-    # Field name made lowercase.
-    menualias = models.CharField(
-        db_column='menuAlias', max_length=50, blank=True, null=True)
-    cost = models.IntegerField(blank=True, null=True)
-    # Field name made lowercase.
-    date = models.DateField(db_column='Date', blank=True, null=True)
-    new_column = models.IntegerField(blank=True, null=True)
-    # Field name made lowercase.
-    storeid = models.ForeignKey(
-        'Store', models.DO_NOTHING, db_column='storeId', blank=True, null=True)
+    commentid = models.AutoField(db_column='commentId', primary_key=True)  # Field name made lowercase.
+    useralias = models.ForeignKey('User', models.DO_NOTHING, db_column='userAlias')  # Field name made lowercase.
+    reviewid = models.ForeignKey('Review', models.DO_NOTHING, db_column='reviewId')  # Field name made lowercase.
+    content = models.CharField(max_length=300)
+    registerdate = models.DateField(db_column='registerDate')  # Field name made lowercase.
 
     class Meta:
         managed = False
         db_table = 'comment'
 
 
+class Like(models.Model):
+    likeid = models.AutoField(db_column='likeId', primary_key=True)  # Field name made lowercase.
+    useralias = models.ForeignKey('User', models.DO_NOTHING, db_column='userAlias')  # Field name made lowercase.
+    storeid = models.ForeignKey('Store', models.DO_NOTHING, db_column='storeId')  # Field name made lowercase.
+    show = models.IntegerField()
+    updatedate = models.DateTimeField(db_column='updateDate')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'like'
+
+
 class Menu(models.Model):
-    # Field name made lowercase.
-    menuid = models.CharField(
-        db_column='menuId', primary_key=True, max_length=50)
-    storeid = models.ForeignKey(
-        'Store', models.DO_NOTHING, db_column='storeId', blank=True)
-    # Field name made lowercase.
-    menualias = models.CharField(
-        db_column='menuAlias', max_length=50, blank=True, null=True)
-    cost = models.IntegerField(blank=True, null=True)
-    # Field name made lowercase.
-    commentid = models.CharField(
-        db_column='commentId', max_length=50, blank=True, null=True)
-    score = models.FloatField(blank=True, null=True)
+    menuid = models.AutoField(db_column='menuId', primary_key=True)  # Field name made lowercase.
+    menualias = models.CharField(db_column='menuAlias', max_length=50)  # Field name made lowercase.
+    storeid = models.ForeignKey('Store', models.DO_NOTHING, db_column='storeId')  # Field name made lowercase.
+    cost = models.IntegerField()
+    choicecount = models.IntegerField(db_column='choiceCount')  # Field name made lowercase.
 
     class Meta:
         managed = False
         db_table = 'menu'
 
 
+class Report(models.Model):
+    reportid = models.AutoField(db_column='reportId', primary_key=True)  # Field name made lowercase.
+    reviewid = models.ForeignKey('Review', models.DO_NOTHING, db_column='reviewId')  # Field name made lowercase.
+    report = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'report'
+
+
+class Review(models.Model):
+    reviewid = models.AutoField(db_column='reviewId', primary_key=True)  # Field name made lowercase.
+    useralias = models.ForeignKey('User', models.DO_NOTHING, db_column='userAlias', blank=True, null=True)  # Field name made lowercase.
+    storeid = models.ForeignKey('Store', models.DO_NOTHING, db_column='storeId')  # Field name made lowercase.
+    menuid = models.ForeignKey(Menu, models.DO_NOTHING, db_column='menuId')  # Field name made lowercase.
+    score = models.FloatField()
+    content = models.CharField(max_length=500)
+    imagepath = models.TextField(db_column='imagePath', blank=True, null=True)  # Field name made lowercase.
+    show = models.IntegerField()
+    registerdate = models.DateField(db_column='registerDate')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'review'
+
+
+class Save(models.Model):
+    saveid = models.AutoField(db_column='saveId', primary_key=True)  # Field name made lowercase.
+    useralias = models.ForeignKey('User', models.DO_NOTHING, db_column='userAlias')  # Field name made lowercase.
+    storeid = models.ForeignKey('Store', models.DO_NOTHING, db_column='storeId')  # Field name made lowercase.
+    show = models.IntegerField()
+    updatedate = models.DateTimeField(db_column='updateDate')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'save'
+
+
 class Store(models.Model):
-    # Field name made lowercase.
-    storeid = models.CharField(
-        db_column='storeId', primary_key=True, max_length=50)
-    address = models.CharField(max_length=50, blank=True, null=True)
-    score = models.FloatField(blank=True, null=True)
-    # Field name made lowercase.
-    commentid = models.CharField(
-        db_column='commentId', max_length=50, blank=True, null=True)
-    category = models.CharField(max_length=50, blank=True)
-    monthlyvote = models.IntegerField(
-        blank=True, null=True, default=0)
-    like = models.IntegerField(blank=True, default=0)
-    gendate = models.DateField(blank=True, null=True)
-    scorecount = models.IntegerField(blank=True)
+    storeid = models.AutoField(db_column='storeId', primary_key=True)  # Field name made lowercase.
+    storealias = models.CharField(db_column='storeAlias', max_length=50)  # Field name made lowercase.
+    address = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    mapx = models.CharField(max_length=20)
+    mapy = models.CharField(max_length=20)
+    score = models.FloatField()
+    scorecount = models.IntegerField(db_column='scoreCount')  # Field name made lowercase.
+    likecount = models.IntegerField(db_column='likeCount')  # Field name made lowercase.
+    savecount = models.IntegerField(db_column='saveCount')  # Field name made lowercase.
+    imagepath = models.TextField(db_column='imagePath', blank=True, null=True)  # Field name made lowercase.
+    registerdate = models.DateField(db_column='registerDate')  # Field name made lowercase.
+    updatedate = models.DateField(db_column='updateDate')  # Field name made lowercase.
 
     class Meta:
         managed = False
         db_table = 'store'
+        unique_together = (('storeid', 'storealias'),)
+
+
+class Support(models.Model):
+    supportid = models.AutoField(db_column='supportId', primary_key=True)  # Field name made lowercase.
+    useralias = models.ForeignKey('User', models.DO_NOTHING, db_column='userAlias')  # Field name made lowercase.
+    type = models.CharField(max_length=4)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    reviewid = models.ForeignKey(Review, models.DO_NOTHING, db_column='reviewId', blank=True, null=True)  # Field name made lowercase.
+    content = models.TextField()
+    registerdate = models.DateField(db_column='registerDate')  # Field name made lowercase.
+    reply = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'support'
 
 
 class User(models.Model):
-    uid = models.CharField(primary_key=True, max_length=50)
-    nickname = models.CharField(
-        unique=True, max_length=50, blank=True, null=True)
-    userlevel = models.CharField(max_length=20, blank=True, null=True)
-    # Field name made lowercase.
-    postid = models.CharField(
-        db_column='postId', max_length=20, blank=True, null=True)
-    path = models.CharField(max_length=100, blank=True, null=True)
-    # Field name made lowercase.
-    registerdate = models.DateField(db_column='registerDate')
+    uid = models.CharField(db_column='uId', max_length=50)  # Field name made lowercase.
+    useralias = models.CharField(db_column='userAlias', primary_key=True, max_length=50)  # Field name made lowercase.
+    userlevel = models.CharField(db_column='userLevel', max_length=20)  # Field name made lowercase.
+    imagepath = models.TextField(db_column='imagePath', blank=True, null=True)  # Field name made lowercase.
+    registerdate = models.DateField(db_column='registerDate')  # Field name made lowercase.
 
     class Meta:
         managed = False
